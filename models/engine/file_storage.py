f7ddd151f748cb451d06799bc38c6de1c1fdee01
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-07-18 21:55:44 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,39 +57,16 @@ class FileStorage:
             FileStorage.__objects = obj_dict
 
     def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+     """Returns the valid attributes and their types for each classname"""
+     return {
+        "BaseModel": {"id": str, "created_at": datetime.datetime, "updated_at": datetime.datetime},
+        "User": {"email": str, "password": str, "first_name": str, "last_name": str},
+        "State": {"name": str},
+        "City": {"state_id": str, "name": str},
+        "Amenity": {"name": str},
+        "Place": {"city_id": str, "user_id": str, "name": str, "description": str,
+                  "number_rooms": int, "number_bathrooms": int, "max_guest": int,
+                  "price_by_night": int, "latitude": float, "longitude": float,
+                  "amenity_ids": list},
+        "Review": {"place_id": str, "user_id": str, "text": str}
+    }
