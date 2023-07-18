e527350f57caa82928480f24a4bedb52fab1efaa
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-07-18 22:00:18 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,6 @@ from models import storage
 
 
 class BaseModel:
-
     """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
@@ -21,13 +20,11 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, "created_at", datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, "updated_at", datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, key, kwargs[key])
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -36,21 +33,19 @@ class BaseModel:
 
     def __str__(self):
         """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        """Updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        """Returns a dictionary containing all keys/values of __dict__"""
+        my_dict = {**self.__dict__}
+        my_dict.update({
+            "__class__": type(self).__name__,
+            "created_at": my_dict["created_at"].isoformat(),
+            "updated_at": my_dict["updated_at"].isoformat()
+        })
         return my_dict
